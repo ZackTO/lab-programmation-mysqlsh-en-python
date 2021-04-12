9d8b9fd7b3ecfcdb73f82d3eb2ee7db9fcf7ab03
ZackTO <zackstoronto@gmail.com>
2021-04-11 21:09:33 -0400

    Tutoriel complété

--- b000000000.py
+++ b000000000.py
@@ -44,16 +44,34 @@ def lecture(fichier):
   # Retourne un dictionnaire Python du fichier json converti
   return docs
 
+  
+
+def former_des_chefs(docs):
+
+  # Crée une nouvelle collection 'chefs_de_gouvernement'
+  nomColl = 'chefs_de_gouvernement'
+  maColl = db.create_collection(nomColl)
+    # Ajout manuel
+  maColl.add({"HeadOfState": "Marc Ravalomanana","GovernmentForm": "Republic"}).execute()
+
+  # Manipuler la collection et la rajouter à la nouvelle
   for doc in docs.fetch_all():
     for country in doc.countries:
+      # Insert des documents JSON de type government
+      maColl.add(country['government']).execute()
 
+  # Trouver tous les documents JSON et les mettre en mémoire
+  docs = maColl.find().execute()
 
+  # Détruit la collection
+  #db.drop_collection(nomColl)
+
+  return docs
 
 def main():
   docs = lecture('b000000000.json')
-  print(len(docs.fetch_all()))
-
-
+  chefs = former_des_chefs(docs)
+  print(len(chefs.fetch_all()))
   # Ne pas oublier de remercier le gestionnaire de BD
   session.close
 
